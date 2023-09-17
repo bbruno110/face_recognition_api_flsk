97ffa1591c04ce84cc3d2a02ff19ec1d574d2eb7
bruno santos <bruno.santos6@ftc.edu.br>
2023-09-17 00:06:12 -0300

    att

--- app.py
+++ app.py
@@ -3,20 +3,17 @@ import dlib
 import cv2
 import logging
 import os
+import base64
 import numpy as np
-import time
-from flask_cors import CORS, cross_origin
+import logging
+from flask_cors import CORS
 import uuid  # Importe o módulo uuid
 
 app = Flask(__name__)
 CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-logging.basicConfig(level=logging.DEBUG)
-logger = app.logger
-file_handler = logging.FileHandler('app.log')
-file_handler.setLevel(logging.DEBUG)
-app.logger.addHandler(file_handler)
-
+logging.basicConfig(filename='flask_app.log', level=logging.DEBUG)
 # Carregue seus modelos e descritores faciais aqui
 face_detector = dlib.get_frontal_face_detector()
 face_recognizer = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
@@ -35,60 +32,105 @@ def allowed_file(filename):
 
 @app.route('/detect_teeth', methods=['POST'])
 def detect_teeth():
-    logger.debug('Acessando a rota principal')
-    # Verifique se a solicitação possui uma imagem
     if 'photo' not in request.files:
         return jsonify({'message': 'Nenhuma imagem enviada'}), 400
 
     photo = request.files['photo']
 
-    # Verifique se a extensão da imagem é válida
     if not allowed_file(photo.filename):
         return jsonify({'message': 'Extensão de arquivo não suportada'}), 400
 
-    # Gere um nome de arquivo único usando um UUID
-    unique_filename = str(uuid.uuid4()) + '.jpg'
-    photo_path = os.path.join(img_folder, unique_filename)
-    photo.save(photo_path)
+    img_bytes = photo.read()
+    img_np = np.frombuffer(img_bytes, np.uint8)
+    img = cv2.imdecode(img_np, cv2.IMREAD_COLOR)
 
-    # Carregue a imagem e execute a detecção de dentes
-    img = cv2.imread(photo_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector(gray, 1)
 
     if len(faces) > 0:
         for face in faces:
-            # Encontre os pontos de referência do rosto
             shape = shape_predictor(gray, face)
-
-            # Calcule a distância entre os pontos de referência dos lábios superior e inferior
             top_lip = shape.part(62).y
             bottom_lip = shape.part(66).y
-            lip_distance = bottom_lip - top_lip
+
+            # Calcule o ponto médio entre os lábios
+            lip_midpoint = (top_lip + bottom_lip) // 2
 
             # Defina um limiar para determinar se os dentes estão visíveis ou não
-            lip_distance_threshold = 1
+            lip_distance_threshold = 20  # Ajuste este valor conforme necessário
 
-            # Desenhe um contorno verde ao redor dos dentes se eles estiverem visíveis, caso contrário, retorne uma mensagem
             teeth_points = shape.parts()[60:68]
             teeth_points = np.array([[p.x, p.y] for p in teeth_points])
+            
+            # Calcule a distância entre o ponto médio dos lábios e o lábio inferior
+            lip_distance = abs(bottom_lip - lip_midpoint)
+
             if lip_distance > lip_distance_threshold:
                 cv2.drawContours(img, [teeth_points], -1, (0, 255, 0), 2)
             else:
-                return jsonify({'message': 'Nenhum dente encontrado'}), 200  # Retorna a mensagem quando os dentes não são visíveis
+                return jsonify({'message': 'Nenhum dente encontrado'}), 206
 
-        # Salve a imagem com os contornos dos dentes desenhados
+        unique_filename = str(uuid.uuid4()) + '.jpg'
+        photo_path = os.path.join(img_folder, unique_filename)
         cv2.imwrite(photo_path, img)
 
-        # Retorna a imagem resultante com o nome original como resposta
-        return send_file(photo_path, mimetype='image/jpeg'), 200
-    else:
-        return jsonify({'message': 'Nenhum rosto detectado na imagem'}), 400
+        with open(photo_path, "rb") as image_file:
+            encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
 
+        # Remova a imagem após enviá-la em base64
+        os.remove(photo_path)
+
+        return jsonify({'image': encoded_image}), 200
+    else:
+        return jsonify({'message': 'Nenhum rosto detectado na imagem'}), 204
+
+@app.route('/convert_to_jpg', methods=['POST'])
+def convert_to_jpg():
+    try:
+        data = request.json  # Assume que a imagem em base64 é enviada como um objeto JSON com a chave 'base64_image'
+
+        if 'base64_image' not in data:
+            return jsonify({'message': 'Base64 image not found in request'}), 400
+
+        base64_image = data['base64_image']
+
+        # Verifique se a string base64 começa com o cabeçalho "data:image/jpeg;base64,"
+        if base64_image.startswith("data:image/jpeg;base64,"):
+            # Remova o cabeçalho para obter apenas os dados de imagem em base64
+            base64_image = base64_image[len("data:image/jpeg;base64,"):]
+
+        # Decodificar a imagem em base64
+        image_data = base64.b64decode(base64_image)
+
+        # Verifique se image_data não está vazio
+        if not image_data:
+            return jsonify({'message': 'Invalid base64 image data'}), 400
+
+        # Gerar um nome de arquivo único
+        unique_filename = str(uuid.uuid4()) + '.jpeg'
+
+        # Defina o caminho para salvar a imagem jpg na pasta "imgs"
+        jpg_filename = os.path.join(img_folder, unique_filename)
+
+        # Salve a imagem como jpg
+        with open(jpg_filename, 'wb') as img_file:
+            img_file.write(image_data)
+
+        # Retorne a imagem como parte da resposta
+        return send_file(jpg_filename, mimetype='image/jpeg')
+
+    except Exception as e:
+        # Registre a exceção no arquivo de log
+        app.logger.exception("Erro durante o processamento da requisição:")
+
+        # Retornar uma resposta de erro (status 500) em JSON
+        return jsonify({'error': str(e)}), 500
+
+   
 @app.route('/', methods=['GET'])
 def ping():
-    logger.debug('Acessando a rota principal')
     return jsonify({'message': 'pong'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+      app.run(host='0.0.0.0', port=5000)
